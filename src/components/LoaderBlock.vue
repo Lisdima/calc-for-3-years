bf92d8e0b9969d1d7a4cedec88b3cc24f56ee39a
Dmitriy Lisyanskiy <d.lisyansky@efit.biz>
2022-05-27 11:15:59 +0300

    fix svg for tilda

--- src/components/LoaderBlock.vue
+++ src/components/LoaderBlock.vue
@@ -37,12 +37,12 @@ export default {
     top: 50%;
     left: 50%;
     transform: translate(-50%, 0);
+    svg {
+      width: 100px;
+      height: 100px;
+      margin: 20px;
+      display: inline-block;
+    }
   }
-}
-svg {
-  width: 100px;
-  height: 100px;
-  margin: 20px;
-  display: inline-block;
 }
 </style>
